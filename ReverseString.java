335ad45f1499a71486943e30b45b71bf986631c4
jas04-11 <140260520+jas04-11@users.noreply.github.com>
2025-10-30 20:11:50 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+/*Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0, right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        
+        }
+    }
+}
